aab09ed79c8e565fa9b5d7b60f8f511fd474bcf1
adrien <azinger@eos-imaging.com>
2021-05-11 10:09:16 +0200

    Add Find index implementation

--- src/StatToolImpl.cpp
+++ src/StatToolImpl.cpp
@@ -1,7 +1,33 @@
 #include "StatToolImpl.hpp"
+#include <cmath>
+
+//#include <iostream>
 
 bool StatToolImpl::FindIndex(const std::vector<int> &vec, const int time_ref, const bool left, int &index) {
-    return false;
+    int l = 0;
+    int r = vec.size() - 1;
+    if (vec[l] > time_ref || vec[r] < time_ref || vec[l] >= vec[r])
+        return false;
+    if (vec[l] == time_ref) {
+        index = l;
+        return true;
+    }
+    if (vec[r] == time_ref) {
+        index = r;
+        return true;
+    }
+    while (r - l > 1) {
+        const int m = std::floor((l+r) / 2);
+        if (vec[m] == time_ref) {
+            index = m;
+            return true;
+        }
+        if (vec[l] <= time_ref && time_ref < vec[m])
+            r = m;
+        else l = m;
+    }
+    index = left ? l : r;
+    return true;
 }
 
 bool StatToolImpl::Sum(const std::vector<int> &vec, int &sum) {
--- tests/StatToolImplTests.cpp
+++ tests/StatToolImplTests.cpp
@@ -7,10 +7,30 @@ TEST(StatToolImplTest, FindIndexExact) {
     int index;
     EXPECT_TRUE(tool.FindIndex(vec, 3, true, index));
     EXPECT_EQ(2, index);
+    EXPECT_TRUE(tool.FindIndex(vec, 3, false, index));
+    EXPECT_EQ(2, index);
+    EXPECT_TRUE(tool.FindIndex(vec, 9, true, index));
+    EXPECT_EQ(8, index);
     EXPECT_TRUE(tool.FindIndex(vec, 9, false, index));
     EXPECT_EQ(8, index);
+}
+
+TEST(StatToolImplTest, FindIndexExactExtremities) {
+    StatToolImpl tool;
+    std::vector<int> vec{1,2,3,4,5,6,7,8,9,10};
+    int index;
     EXPECT_TRUE(tool.FindIndex(vec, 1, false, index));
     EXPECT_EQ(0, index);
     EXPECT_TRUE(tool.FindIndex(vec, 10, false, index));
     EXPECT_EQ(9, index);
+}
+
+TEST(StatToolImplTest, FindInBetween) {
+    StatToolImpl tool;
+    std::vector<int> vec{1,3,5,6,8,9,11,15,17,18};
+    int index;
+    EXPECT_TRUE(tool.FindIndex(vec, 10, true, index));
+    EXPECT_EQ(5, index);
+    EXPECT_TRUE(tool.FindIndex(vec, 10, false, index));
+    EXPECT_EQ(6, index);
 }
